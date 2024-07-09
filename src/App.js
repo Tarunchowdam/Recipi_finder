51f8b4a131f02ff4ae2c76b21fc4c7b8417e525c
Tarunchowdam <tarunchowdam435@gmail.com>
2024-07-09 16:24:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+import NavBar from './components/Navbar';
+import Home from './Pages/Home';
+import Recipes from './Pages/Recipes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RecipeDetails from './components/RecipeDetails';
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/constants/constant.js
@@ -0,0 +1,4 @@
+export const logo = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRvnyjCa80RxhHcPbO5JVckEJ4JHkPC0R2-p6DSYP6aw&s';
+
+
+export const API_URL='https://forkify-api.herokuapp.com/api';
